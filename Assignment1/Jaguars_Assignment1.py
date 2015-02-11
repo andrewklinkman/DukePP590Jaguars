9233d22d1bc0362d7b9f08f11a14cd5f38b6d31c
andrewklinkman <andrew.klinkman@duke.edu>
2015-02-10 23:39:27 -0500

    Final Push for A1
    drops ‘\s’ to make import faster, also drops DST days

--- Assignment1/Jaguars_Assignment1.py
+++ Assignment1/Jaguars_Assignment1.py
@@ -1,11 +1,17 @@
+from __future__ import division
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
 import os
-import datetime as dt
+
 
 #IMPORT AND CLEAN CER DATA
+
+#this dir is for windows computers/cluster
 main_dir = "C:/Users/ask46/Dropbox/Duke/School Stuff 14-15/Spring 2015/PUBPOL 590/CER Stuff/raw/"
+
+#this dir is for my mac
+#main_dir = "/Users/andrewklinkman/Dropbox/Duke/School Stuff 14-15/Spring 2015/PUBPOL 590/CER Stuff/raw/"
 csv_file = "SME and Residential allocations.csv"
 
 #build file list
@@ -13,10 +19,7 @@ all_files = [os.path.join(main_dir, v) for v in os.listdir(main_dir) if v.starts
 
 #turn all files in list into dataframes
 #and build list out of all of them
-all_dfs = [pd.read_table(v, sep = '\s', names = ['meterid', 'timedate', 'consump']) for v in all_files]
-
-#all_dfs[1]
-#df1 = pd.read_table(all_files, sep = '\s', names = ['meterid', 'timedate', 'consump'])
+all_dfs = [pd.read_table(v, sep = ' ', names = ['meterid', 'timedate', 'consump']) for v in all_files]
 
 #merge all the existing dataframes from the text files
 df1 = pd.concat(all_dfs, ignore_index = True)
@@ -37,15 +40,26 @@ df1 = None #memory mgmt
 df_def = None #memory mgmt
 
 #drop all 'study drop-outs' where code = 3 (i.e. participant didn't complete study)
-df_combo = df_combo[df_combo.Code != 3]
+df_combo = df_combo[df_combo.Code != 3].copy()
 
 #verify that all Code 3s have been dropped
 sum(df_combo.Code[df_combo.Code==3])
 
+#DROP ALL DST DATES
+#make df_combo equal to itself, excluding the three DST days
+df_combo = df_combo[
+    ((df_combo.timedate-(df_combo.timedate % 100))/100 != [298]) &
+    ((df_combo.timedate-(df_combo.timedate % 100))/100 != [452]) &
+    ((df_combo.timedate-(df_combo.timedate % 100))/100 != [669]) 
+].copy()
+
+#check to make sure they dropped
+df_combo[df_combo.timedate==29850]
+
 #check for duplicated rows on subset of meterid and timedate
 sum(df_combo.duplicated(subset = ['meterid', 'timedate']))
 #True = 1, False = 0, sum of array = 0, so no duplicates!
-
+#NOTE: this takes FOREVER...save this command for the cluster
 
 #check datatypes
 df_combo.dtypes #all good, no random periods or dashes. 
@@ -61,15 +75,4 @@ df_combo[rows2]
 df_combo[rows3]
 #doesn't look like we have any
 
-#---------------------------------------------------------------------------
-##convert dates
-#base_date = dt.datetime.strptime("2009-01-01", "%Y-%m-%d")
-#
-##get first three chars of time field (days)
-#temp1 = str(df1[11:12]['timedate'])
-#intTemp1=int(temp1[5:8])
-#
-##get last two chars of time field (half hour increments)
-#intTemp2=int(temp1[9:11])
-#
-#actual_date = base_date + dt.timedelta(days=intTemp1) + dt.timedelta(minutes = intTemp2*30)
+
